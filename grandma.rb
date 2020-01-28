cf487455be7b9d402da330ec318e807a3de7ef00
Somaia Khalil <sumaya_khair@live.com>
2020-01-28 04:13:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase == I "LOVE YOU GRANDMA!"
-    return ""
+    return "I LOVE YOU TOO PUMPKIN!"
   elseif  
     "HUH?! SPEAK UP, SONNY!"
   else 
