54ace70d2c7a0cafc1c1d845d8d49fe059ad1ba9
Somaia Khalil <sumaya_khair@live.com>
2020-01-28 04:17:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
-  if phrase != phrase.upper_case()
+  if phrase != phrase.uppercase()
     "HUH?! SPEAK UP, SONNY!"
   else 
     ""
